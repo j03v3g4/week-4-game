f5922be600ffc17bf06a8454301cf67150b5dea6
Joseph Vega <1josephvega@gmail.com>
2018-02-28 17:47:40 -0500

    Appends for target number, user number, total wins, and total losses. Reset function created for use in new victory and defeat functions.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,4 +11,34 @@ $( document ).ready(function(){
     gemTwo = Math.floor(Math.random()*11+1);
     gemThree = Math.floor(Math.random()*11+1);
     gemFour = Math.floor(Math.random()*11+1);
+    // Append initial target and score values to their respective html spans
+    $('#userNumber').text(userNumber);
+    $('#randomNumber').text(randomNumber);
+    $('#totalWins').text(wins);
+    $('#totalLosses').text(losses);
+    // Function to reset the game on start, after win, or after a loss
+    function reset() {
+        userNumber = 0;
+        randomNumber = Math.floor(Math.random()*101+19);
+        gemOne = Math.floor(Math.random()*11+1);
+        gemTwo = Math.floor(Math.random()*11+1);
+        gemThree = Math.floor(Math.random()*11+1);
+        gemFour = Math.floor(Math.random()*11+1);
+        $('#userNumber').text(userNumber);
+        $('#randomNumber').text(randomNumber);      
+    }
+    // Victory function to update total wins, alert the user, and reset the game
+    function victory() {
+        wins++;
+        $('#totalWins').text(wins);
+        alert("VICTORY!");
+        reset();
+    }
+    // Defeat function to update total losses, alert the user, and reset the game
+    function defeat() {
+        wins++;
+        $('#totalLosses').text(losses);
+        alert("DEFEAT!");
+        reset();
+    }
 });
